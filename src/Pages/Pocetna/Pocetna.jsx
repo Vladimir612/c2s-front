f025bed1291ac4e5256b98cf3c191dfba1eb84b7
Vladimir Lazarevic <vladimir.12.lazarevic@gmail.com>
2022-03-31 17:36:39 +0200

    Dodate animacije sa loptama

--- src/Pages/Pocetna/Pocetna.jsx
+++ src/Pages/Pocetna/Pocetna.jsx
@@ -9,69 +9,81 @@ const Pocetna = () => {
   return (
     <div className="pocetna-container">
       <Nav />
-      <div className="lopte">
-        <div className="prva-grupa">
-          <Lopta boja="bela" width="3rem" height="3rem" />
-          <Lopta boja="crvena" width="5rem" height="5rem" />
+      <div className="hero-section">
+        <div className="lopte">
+          <div className="prva-grupa">
+            <Lopta boja="bela" width="3rem" height="3rem" />
+            <Lopta boja="crvena" width="5rem" height="5rem" />
+          </div>
+          <div className="druga-grupa">
+            <Lopta boja="bela" width="4.5rem" height="4.5rem" />
+            <Lopta boja="crvena" width="8rem" height="8rem" />
+          </div>
+          <div className="treca-grupa">
+            <Lopta boja="transparentna" width="4.5rem" height="4.5rem" />
+            <Lopta boja="crvena" width="5rem" height="5rem" />
+          </div>
+        </div>
+        <div className="content">
+          <div className="staklo-container">
+            <h1>kompanije studentima</h1>
+            <h2>
+              break your lim<span>it</span>s
+            </h2>
+            <p className="uvodni-tekst">
+              Kompanije studentima je projekat Udruženja studenata informatike
+              FONIS koji za cilj ima prenošenje znanja i povezivanje studenata
+              sa IT kompanijama. Ove godine će doživeti izdanje obojeno novim
+              bojama, obogaćen sadržajem koji će svim studentima pružiti
+              priliku.
+            </p>
+            <div className="timer">
+              <div className="dani">
+                <p>14</p>
+                <p>dana</p>
+              </div>
+              <div className="sati">
+                <p>08</p>
+                <p>sati</p>
+              </div>
+              <div className="minuti">
+                <p>15</p>
+                <p>minuta</p>
+              </div>
+            </div>
+            <div className="dugmad-grupa">
+              <motion.button
+                className="dugme crveno"
+                whileHover={{
+                  scale: 1.1,
+                }}
+                whileTap={{
+                  scale: 0.9,
+                }}
+              >
+                Prijavi se
+              </motion.button>
+              <motion.button
+                className="dugme transparentno"
+                whileHover={{
+                  scale: 1.1,
+                }}
+                whileTap={{
+                  scale: 0.9,
+                }}
+              >
+                O projektu
+              </motion.button>
+            </div>
+          </div>
+          <div className="staklo-container">
+            <div className="img-wrapper">
+              <img src={c2sSvetli} alt="c2s logo" />
+            </div>
+          </div>
         </div>
       </div>
-      <div className="content">
-        <div className="staklo-container">
-          <h1>kompanije studentima</h1>
-          <h2>
-            break your lim<span>it</span>s
-          </h2>
-          <p className="uvodni-tekst">
-            Kompanije studentima je projekat Udruženja studenata informatike
-            FONIS koji za cilj ima prenošenje znanja i povezivanje studenata sa
-            IT kompanijama. Ove godine će doživeti izdanje obojeno novim bojama,
-            obogaćen sadržajem koji će svim studentima pružiti priliku.
-          </p>
-          <div className="timer">
-            <div className="dani">
-              <p>14</p>
-              <p>dana</p>
-            </div>
-            <div className="sati">
-              <p>08</p>
-              <p>sati</p>
-            </div>
-            <div className="minuti">
-              <p>15</p>
-              <p>minuta</p>
-            </div>
-          </div>
-          <div className="dugmad-grupa">
-            <motion.button
-              className="dugme crveno"
-              whileHover={{
-                scale: 1.1,
-              }}
-              whileTap={{
-                scale: 0.9,
-              }}
-            >
-              Prijavi se
-            </motion.button>
-            <motion.button
-              className="dugme transparentno"
-              whileHover={{
-                scale: 1.1,
-              }}
-              whileTap={{
-                scale: 0.9,
-              }}
-            >
-              O projektu
-            </motion.button>
-          </div>
-        </div>
-        <div className="staklo-container">
-          <div className="img-wrapper">
-            <img src={c2sSvetli} alt="c2s logo" />
-          </div>
-        </div>
-      </div>
+      <div className="kompanije-section"></div>
     </div>
   );
 };
