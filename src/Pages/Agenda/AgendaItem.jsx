264635f18c7da5a5838ca03437cdad716d014521
Vladimir Lazarevic <vladimir.12.lazarevic@gmail.com>
2022-04-10 19:30:33 +0200

    Dodate animacije na agendu

--- src/Pages/Agenda/AgendaItem.jsx
+++ src/Pages/Agenda/AgendaItem.jsx
@@ -1,27 +1,52 @@
-import React from 'react'
-import "./AgendaItem.scss"
+import React from "react";
+import "./AgendaItem.scss";
+
+import VisibilitySensor from "react-visibility-sensor";
+import { motion } from "framer-motion";
+import { useState } from "react";
 
 const AgendaItem = (props) => {
+  const [visibleAgendaItem, setVisibleAgendaItem] = useState(false);
+
   return (
-    <div className={props.obrnuto ? "agenda-item obrnuto": "agenda-item"}>
+    <VisibilitySensor
+      onChange={(isVisible) => {
+        if (isVisible && !visibleAgendaItem) {
+          setVisibleAgendaItem(true);
+        }
+        return;
+      }}
+      delayedCall
+      partialVisibility
+    >
+      <motion.div
+        className={props.obrnuto ? "agenda-item obrnuto" : "agenda-item"}
+        animate={visibleAgendaItem && { y: 0, opacity: 1 }}
+        initial={{
+          y: 80,
+          opacity: 0,
+        }}
+        transition={{ duration: 1 }}
+      >
         <div className="left-content">
-            <h3>{props.datum}</h3>
-            <p className="mesto">ikonica {props.mesto}</p>
+          <h3>{props.datum}</h3>
+          <p className="mesto">ikonica {props.mesto}</p>
         </div>
         <div className="right-content">
-            <ul>
-            {
-                props.raspored.map((rasporedEl, index)=> {
-                    return <li key={index}>
-                        <span>{rasporedEl.vreme}</span>
-                        <span>{rasporedEl.desavanje}</span>
-                    </li>
-                })
-            }
-            </ul>
+          <ul>
+            {props.raspored.map((rasporedEl, index) => {
+              return (
+                <li key={index}>
+                  <span>{rasporedEl.vreme}</span>
+                  <span>{rasporedEl.desavanje}</span>
+                </li>
+              );
+            })}
+          </ul>
         </div>
-    </div>
-  )
-}
+      </motion.div>
+    </VisibilitySensor>
+  );
+};
 
-export default AgendaItem
+export default AgendaItem;
