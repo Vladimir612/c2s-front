4fdee94353adc9d48a4b8c801713858768cbdc0c
dusank01 <dusankostic01@gmail.com>
2022-04-12 00:30:41 +0200

    Radio na nama

--- src/Pages/ONama/ONama.jsx
+++ src/Pages/ONama/ONama.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./ONama.scss";
+import nekiLjudi from "../../Assets/Images/ljudi.png";
+import Footer from "../../Components/Footer/Footer";
+import Nav from "./../../Components/Nav/Nav";
 
 const ONama = () => {
   return (
-    <div>
-      <div className="logo-c2s">
+    <>
+    <Nav />
+    <div className="o-nama-container">
+      {/* <div className="logo-c2s">
         <div className="levi-trapez"></div>
         <div className="srednji-trapez"></div>
         <div className="desni-trapez"></div>
@@ -23,8 +28,59 @@ const ONama = () => {
           <div className="lin linija-4"></div>
           <div className="lin linija-5"></div>
         </div>
-      </div>
+      </div> */}
+      <div class="o-nama-flex">
+            <div className="left-content">
+                <h2>O FONISU</h2>
+                <p>FONIS predstavlja ujedinjujuću tačku studenata informatike i mesto za generisanje novih i širenje postojećih znanja iz oblasti informacionih tehnologija, a takođe i mesto na kom se stvaraju nova poznanstva koja često prerastaju u neraskidiva prijateljstva.
+                    Interne radionice našim članovima daju jedinstvenu priliku da svoja postojeća IT i menadžment znanja i veštine upotpune novim i primene ih pre svega na internim hakatonima, ali i kroz realizaciju projekata poput FON Hakatona, Hakatona za srednjoškolce i Studenti studentima koje dugi niz godina organizujemo.</p>
+            </div>
+            <div className="right-content">
+              <div class="img-wrapper">
+                  <img src={nekiLjudi} alt="fonis-slika" />
+              </div>
+            </div>
+        </div>
+        <div class="o-nama-flex">
+            <div className="left-content">
+                <h2>O FONISU</h2>
+                <p>FONIS predstavlja ujedinjujuću tačku studenata informatike i mesto za generisanje novih i širenje postojećih znanja iz oblasti informacionih tehnologija, a takođe i mesto na kom se stvaraju nova poznanstva koja često prerastaju u neraskidiva prijateljstva.
+                    Interne radionice našim članovima daju jedinstvenu priliku da svoja postojeća IT i menadžment znanja i veštine upotpune novim i primene ih pre svega na internim hakatonima, ali i kroz realizaciju projekata poput FON Hakatona, Hakatona za srednjoškolce i Studenti studentima koje dugi niz godina organizujemo.</p>
+            </div>
+            <div className="right-content">
+              <div class="img-wrapper">
+                  <img src={nekiLjudi} alt="fonis-slika" />
+              </div>
+            </div>
+        </div>
+        {/* <div class="o-nama-timovi">
+            <h2>ORGANIZACIONI TIM PROJEKTA</h2>
+            <div class="o-nama-carousel">
+                <div class="o-nama-navigation">
+
+                </div>
+                <div class="o-nama-content">
+                    <div class="o-nama-content-text">
+                        <h2>CORE TIM</h2>
+                        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Adipisci incidunt, tempora iure veniam, sint, officiis nostrum distinctio at dolorem facere ab reiciendis delectus id odit error eveniet culpa hic beatae!</p>
+                        <div class="o-nama-coordinator">
+                            <div class="o-nama-coordinator-img">
+                                <img src="" alt="">
+                            </div>
+                            <div class="o-nama-coordinator-text">
+
+                            </div>
+                        </div>
+                    </div>
+                    <div class="o-nama-content-img">
+                        <img src="" alt=""/>
+                    </div>
+                </div>
+            </div>
+        </div> */}
     </div>
+    <Footer />
+    </>
   );
 };
 
