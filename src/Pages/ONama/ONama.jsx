132b29ee4d1938297f758e5f844e8c4bf231b2e1
Vladimir Lazarevic <vladimir.12.lazarevic@gmail.com>
2022-04-11 23:14:20 +0200

    Napravljena logo navigacija, iskoristiti je u slideru

--- src/Pages/ONama/ONama.jsx
+++ src/Pages/ONama/ONama.jsx
@@ -1,7 +1,31 @@
 import React from "react";
+import "./ONama.scss";
 
 const ONama = () => {
-  return <div>ONama</div>;
+  return (
+    <div>
+      <div className="logo-c2s">
+        <div className="levi-trapez"></div>
+        <div className="srednji-trapez"></div>
+        <div className="desni-trapez"></div>
+        <div className="lopte-nav">
+          <div className="lop lopta-1 active">1</div>
+          <div className="lop lopta-2">2</div>
+          <div className="lop lopta-3">3</div>
+          <div className="lop lopta-4">4</div>
+          <div className="lop lopta-5">5</div>
+          <div className="lop lopta-6">6</div>
+        </div>
+        <div className="linije-nav">
+          <div className="lin linija-1"></div>
+          <div className="lin linija-2"></div>
+          <div className="lin linija-3"></div>
+          <div className="lin linija-4"></div>
+          <div className="lin linija-5"></div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ONama;
